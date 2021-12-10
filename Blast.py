18f9930a011f132e31b20fe48ee4bb1c3e8b9c60
JennyWYJ <61251036+JennyWYJ@users.noreply.github.com>
2021-12-10 18:41:41 -0500

    3 db permuts with control

--- Blast.py
+++ Blast.py
@@ -368,21 +368,21 @@ def permutate_indel(numPerms, datab):
         
 def permutate_datab(numPerms, seqProbs):
     datab = random_seq(seqProbs)
-    dbname = create_fasta_datab(datab, 'permutedb', 1)
+    dbname = create_fasta_datab(datab, 'permutedb3', 1)
     create_datab(dbname)
 
-    create_query_file("control_for_db_perm", "Control")
-    create_query("control_for_db_perm", 1, datab, True)
+    create_query_file("control_for_db_perm3", "Control")
+    create_query("control_for_db_perm3", 1, datab, True)
 
     result = open("Results/control_for_db_perm_results.txt", "w")
 
     for i in range(numPerms):
         result.write("DATABASE"+str(i)+'\n')
-        run_BLASTn("ControlQueries/control_for_db_perm.fasta", dbname, 'controldb_out.xml')
-        parse_XML('controldb_out.xml','control_for_db_perm_results', seqProbs, datab)
+        run_BLASTn("ControlQueries/control_for_db_perm3.fasta", dbname, 'controldb3_out.xml')
+        parse_XML('controldb3_out.xml','control_for_db_perm3_results', seqProbs, datab)
 
         datab = random_seq(seqProbs)
-        dbname = create_fasta_datab(datab, 'permutedb', 1)
+        dbname = create_fasta_datab(datab, 'permutedb3', 1)
         create_datab(dbname)
 
     result.close()
@@ -421,7 +421,7 @@ def test_indelq():
 
 def test_db_perms():
     seq = sequence_probs('full_probs.txt', 'full_seq.txt')
-    permutate_datab(10, seq)
+    permutate_datab(3, seq)
 
 #test_controlq()
 #test_mutq()
